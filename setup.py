72d5361d87892bdfa0b503957a49032bfcdafba5
andrei-1995 <andrei4o1995@gmail.com>
2016-05-25 17:41:49 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SantaseCardGame",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['python...'],
+    author="Andrei Ivanov",
+    author_email="andrei4o1995@gmail.com",
+    description="Card game",
+    license="GNU GPL v2",
+    keywords="card game",
+    url="https://github.com/andrei-1995/SantaseCardGame",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: End Users/Desktop",
+        "License ::GNU GPL v2",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Games/Entertainment"
+    ]
+)
